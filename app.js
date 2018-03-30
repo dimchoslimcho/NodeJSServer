0f52f77ae125e3cf477a0a4ecdf35d0ab9c95f9a
Dimitar Bochvarovski <dimitar.bachvarovski@yahoo.com>
2018-03-31 00:43:23 +0200

    Express Sessions Part Two

--- app.js
+++ app.js
@@ -7,8 +7,8 @@ var session = require('express-session');
 var FileStore = require('session-file-store')(session);
 
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var index = require('./routes/index');
+var users = require('./routes/users');
 var dishRouter = require('./routes/dishRouter');
 var promoRouter = require('./routes/promoRouter');
 var leaderRouter = require('./routes/leaderRouter');
@@ -33,6 +33,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 app.use(logger('dev'));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false })); //this
 //app.use(cookieParser('12345-67890-09876-54321')); //this
@@ -44,52 +45,34 @@ app.use(session({
   store: new FileStore()
 }));
 
+app.use('/', index); // we are first singing up and after that the auth process is completed not vice versa
+app.use('/users', users); //they must be here
 
 function auth(req, res, next) {
   console.log(req.session);
 
   if(!req.session.user){
-    var authHeader = req.headers.authorization;
-    if(!authHeader) {
-      var err = new Error('You are not authenticated!');
-      res.setHeader('WWW-Authenticate', 'Basic');
-      err.status = 401;
-      return next(err);
-    }
-
-    var auth = new Buffer(authHeader.split(' ')[1], 'base64').toString().split(':') //This is spliting the string from the header which is the authentication message
-    var username = auth[0];
-    var password = auth[1];                                                          // the space is used to determine where we split the string
-                                                              //So for example when the function finds white space it will split the message
-    if(username === 'admin' && password === 'password'){        // and the second part of the message will go to [1]
-      req.session.user = 'admin';                                                        // We are doing two splits here, the second is to split the username and password
-      next(); // Allow the client request to pass to the next middleware
-    }
-    else{
-      var err = new Error('You are not authenticated!');
-      res.setHeader('WWW-Authenticate', 'Basic');
-      err.status = 401;
-      return next(err);
-    }
+    var err = new Error('You are not authenticated!');
+    err.status = 403;
+    return next(err);
   }
   else {
-    if(req.session.user === 'admin'){
+    if(req.session.user === 'authenticated'){
       next();
     }
     else {
       var err = new Error('You are not authenticated!');
-      err.status = 401;
+      err.status = 403;
       return next(err);
     }
   }
-
 }
 app.use(auth);
 
 app.use(express.static(path.join(__dirname, 'public'))); // express is using this to serve static data from our public folder
 // This order has a big importance because every funciotn call is different middleware
-app.use('/', indexRouter); // and this
-app.use('/users', usersRouter);
+/*app.use('/', indexRouter);    //if this two methods(the one below) stay here the app.js wont work the must go before the auth method the
+app.use('/users', usersRouter);*/
 app.use('/dishes', dishRouter);
 app.use('/promotions', promoRouter);
 app.use('/leaders', leaderRouter);
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,94 @@
 var express = require('express');
+const bodyParser = require('body-parser');
+var User = require('../models/user');
+
+
 var router = express.Router();
+router.use(bodyParser.json());
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
+});
+
+router.post('/signup', (req, res, next) => {
+
+  User.findOne({username:req.body.username}) //Checking if already exist this user
+  .then((user) => {
+    if(user != null) {
+      var err = new Error('User '+req.body.username+' already exsists!');
+      err.status = 403;
+      next(err);
+    }
+    else {
+      return User.create({
+        username: req.body.username,
+        password:  req.body.password
+      });
+    }
+  })
+  .then((user) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/json');
+    res.json({status: 'Registration Successful', user: user});
+  }, (err) => next(err))
+  .catch((err) => next(err));
+});
+
+router.post('/login', (req, res, next) => {
+  if(!req.session.user){
+    var authHeader = req.headers.authorization;
+    if(!authHeader) {
+      var err = new Error('You are not authenticated!');
+      res.setHeader('WWW-Authenticate', 'Basic');
+      err.status = 401;
+      return next(err);
+    }
+
+    var auth = new Buffer(authHeader.split(' ')[1], 'base64').toString().split(':') //This is spliting the string from the header which is the authentication message
+    var username = auth[0];
+    var password = auth[1];                                                          // the space is used to determine where we split the string
+
+    User.findOne({username: username})
+    .then((user) => {
+      if(user.username === username && user.password === password){        // and the second part of the message will go to [1]
+        req.session.user = 'authenticated';                                                        // We are doing two splits here, the second is to split the username and password
+        res.statusCode = 200; // Allow the client request to pass to the next middleware
+        res.setHeader('Content-Type', 'text/plain');
+        res.end('You are autheticated!');
+      }
+      else if(user.password !== password){
+        var err = new Error('Your password is incorect!');
+        err.status = 403;
+        return next(err);
+      }
+      else if(user === null){
+        var err = new Error('User '+username+' does not exist');
+        res.setHeader('WWW-Authenticate', 'Basic');
+        err.status = 401;
+        return next(err);
+      }
+    })
+    .catch((err) => next(err));                                                          //So for example when the function finds white space it will split the message
+  }
+  else {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('You are already authenticated!');
+  }
+});
+
+router.get('/logout', (req, res) => {
+  if(req.session){
+    req.session.destroy();
+    res.clearCookie('session_id') // A way of asking the client to remove the cookie
+    res.redirect('/');
+  }
+  else{
+    var err = new Error('You are not logged in!');
+    err.status = 403; //Frobiden operation
+    next(err);
+  }
 });
 
 module.exports = router;
